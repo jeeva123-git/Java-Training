c0a45d6a37f672cb0982d74226241c6f7cacaca5
Jeevan prasath S <jp6784876@gmail.com>
2026-08-20 22:14:50 +0530

    Create main5.java

---
+++ main5.java
@@ -0,0 +1,19 @@
+public class Main
+{
+	public static void main(String[] args){
+	    String a = "jeevanprasath";
+	    for(int i = 0;i < a.length()-1;i++)
+	    {
+	        for(int j = i+1;j < a.length();j++)
+	        {
+	            if(a.charAt(i) == a.charAt(j))
+	            {
+	                System.out.println(a.charAt(i));
+	                return;
+	            }
+	        }
+	    }
+	    
+	}
+	
+}
