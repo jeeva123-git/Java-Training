7672bec7083064ab01a2f2bf9de3507c689bfff6
Jeevan prasath S <jp6784876@gmail.com>
2026-08-20 22:17:53 +0530

    Add functionality to find first non-repeating character
    Implement a program to find and print the first non-repeating character in a string.

---
+++ main6.java
@@ -0,0 +1,26 @@
+public class Main
+{
+	public static void main(String[] args){
+	    String a = "jeevanprasath";
+	    int c=0;
+	    for(int i = 0;i < a.length();i++)
+	    {
+	        c=0;
+	        for(int j = 0;j < a.length();j++)
+	        {
+	            if(a.charAt(i) == a.charAt(j))
+	            {
+	                c++;
+	               
+	            }
+	        }
+	        if (c==1)
+	        {
+	            System.out.println(a.charAt(i));
+	        }
+	        return;
+	    }
+	    
+	}
+	
+}
